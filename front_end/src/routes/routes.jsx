eeb5093c0513f59f7008e7e2295f3bcb71b83daa
fael0702 <rafacilio00@gmail.com>
2022-03-22 22:35:19 +0000

    terminar rotas

--- front_end/src/routes/routes.jsx
+++ front_end/src/routes/routes.jsx
@@ -4,13 +4,15 @@ import { Inicio } from '../pages/Inicio/Home.jsx'
 import { Comprar } from '../pages/Compras/Comprar.jsx'
 import { Principal } from '../components/Principal/principal.jsx'
 
-export function Rotas() {
+export function Rotas(props) {
     
+    let { id } = props
+
     return(
             <Routes>
                 <Route path='/' element={<Inicio/>}/>
                 <Route path='/cadastro' element={<Cadastro/>}/>
-                <Route path='/comprar/:id' element={<Comprar/>}/>
+                <Route path={`/comprar/:${id}`} element={<Comprar/>}/>
                 <Route path='/principal' element={<Principal/>}/>
             </Routes>
     )
